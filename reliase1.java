e101b77d6eaa2d7b7bd1912ab5655062c0ebc5ab
Vsevolod239 <90320192+Vsevolod239@users.noreply.github.com>
2021-09-22 13:49:55 +0300

    Update reliase1.java

--- reliase1.java
+++ reliase1.java
@@ -2,14 +2,6 @@ import java.util.Scanner;
 public class Main {
     static Scanner sc = new Scanner(System.in);
     public static void main(String[] args) {
-        int s=0;
-        int a;
-        do{
-            a= sc.nextInt();
-            if(a>9&a<100){
-                s+=a/10+a%10;
-            }
-        }while (a/10>=1&a/10<10);
-        System.out.println(s);
+
     }
 }
